18c3974fbfec8fa7f01c1c43e24cd8aa95e1740e
Cristian Celis <cristian.celis02@uptc.edu.co>
2024-05-06 08:51:42 -0500

    Create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, 
+            amet, officiis mollitia earum eveniet sint neque debitis tempore 
+            sequi distinctio, dolorem nobis! Rerum, animi vero sed corrupti 
+            cupiditate, neque vitae repellat temporibus ea asperiores, non 
+            minus necessitatibus pariatur? Facilis, dicta qui? Dolorum in 
+            magnam praesentium minus eligendi magni deserunt nostrum quae 
+            excepturi! Neque ad praesentium in? Necessitatibus veritatis, 
+            minus, deleniti enim rem mollitia ipsam voluptatibus assumenda 
+            esse quasi neque illum. Eum officiis fuga quod iure eaque, 
+            voluptate quas odit saepe tempora, voluptatum quis est eveniet 
+            amet! Veritatis repudiandae aperiam eum? Reiciendis quasi 
+            laboriosam repellendus soluta animi?</p>
+    </div>
+  )
+}

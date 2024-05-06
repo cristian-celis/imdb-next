21a364071bcddca362ccf17c4c591acdae053160
Cristian Celis <cristian.celis02@uptc.edu.co>
2024-05-06 10:19:38 -0500

    Fetch data from TMDB api in the home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
   return (
     <div className='flex dark:bg-gray-600 bg-amber-100 p-4 lg:text-large justify-center
     gap-6'>
-        <NavbarItem title="Trending" param="fetchTreding"/>
+        <NavbarItem title="Trending" param="fetchTrending"/>
         <NavbarItem title="Top Rated" param="fetchTopRated"/>
     </div>
   )
